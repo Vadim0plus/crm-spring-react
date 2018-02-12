0cdd06918d1445365c47f56177c9a75b8a42898a
Vadim Tukhvatullin <vadim0plus@gmail.com>
2018-02-13 00:20:14 +0300

    Added Category object

---
+++ src/main/java/crm/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package crm.repository;
+
+import crm.model.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
